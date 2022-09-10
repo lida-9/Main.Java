72c98f74edcb10e9caa1e0f96223735ee33d3858
Ani Davtyan <ani.davtyan@sabx.com>
2022-09-10 17:02:11 +0400

    Initial commit

---
+++ src/Primitive/Main.java
@@ -0,0 +1,74 @@
+package Primitive;
+public class Main {
+    public static void main(String[] args) {
+
+        // int type
+        int b = 2000;
+        int b1 = 909098890;
+        int b2 = -2147483648;
+        System.out.println("int type ");
+        System.out.println("b is: " + b);
+        System.out.println("b1 is: " + b1);
+        System.out.println("b2 is: " + b2);
+
+        // short type
+        short a1 = 100;
+        short a2 = 1000;;
+        System.out.println("short type example");
+        System.out.println("a1 is: " + a1);
+        System.out.println("a2 is: " + a2);
+
+// byte type
+        byte c1 = 0;
+        byte c2 = 121;
+        byte c3 = -127;
+        System.out.println("byte type example");
+        System.out.println("c1 is: " + c1);
+        System.out.println("c2 is: " + c2);
+        System.out.println("c3 is: " + c3);
+        // long type
+        long d1 = 2147483648L;
+        long d2 = 9223372036854775807L;
+        long d3 = -9223372036854775808L;
+        System.out.println("long type example");
+        System.out.println("d1 is: " + d1);
+        System.out.println("d2 is: " + d2);
+        System.out.println("d3 is: " + d3);
+
+        // float type
+        float f5;
+        f5 = 12.5F;
+        float m5 = -922000003372036800000054775808F;
+        System.out.println("float type example");
+        System.out.println("f5 is: " + f5);
+        System.out.println("m5 is: " + m5);
+
+        // double type
+        double l6;
+        l6 = 148.5;
+        double m6 = -92200000337203680000005477580.4551114;
+        System.out.println("double type example");
+        System.out.println("l6 is: " + l6);
+        System.out.println("m6 is: " + m6);
+
+        //boolean type
+        boolean e;
+        e = false;
+        boolean k = true;
+        System.out.println("boolean type example");
+        System.out.println("e is: " + e);
+        System.out.println("k is: " + k);
+
+        //char type
+        char ch1 = 'I';
+        char ch2 = 'S';
+        char ch3 = 34;
+        System.out.println("char type example");
+        System.out.println("ch1 is: " + ch1);
+        System.out.println("ch2 is: " + ch2);
+        System.out.println("ch3 is: " + ch3);
+
+
+    }
+}
+
